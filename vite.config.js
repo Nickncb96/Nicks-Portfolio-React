72f6fb5721de4aa96a8cdbb2d2f13fca182c09b7
nickncb96 <nicholasncb12@hotmail.com>
2024-03-06 09:07:06 +0000

    updating the vite.config.js to include react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '/@/': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+  esbuild: {
+    jsxInject: `import React from 'react';`,
+  },
+});
